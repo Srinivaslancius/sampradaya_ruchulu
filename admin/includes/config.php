d8da321feae25be6d309cca9e03ba13de125d76a
srinivaslancius <srinivas@lanciussolutions.com>
2017-08-09 17:16:55 +0530

    changed config file

--- admin/includes/config.php
+++ admin/includes/config.php
@@ -9,7 +9,7 @@ if($setcon == 1) {
 	$password = "capital@123";
 	$dbname = "capitalsecure_latest";
 } else {
-	$servername = "192.168.0.101";	
+	$servername = "192.168.0.108";	
 	$username = "root";
 	$password = "root";
 	$dbname = "sampradaya_ruchulu";
