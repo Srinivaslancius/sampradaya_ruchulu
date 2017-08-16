d67b72aa7d568023c216641844e0def2a13e2de0
Srinivaslancius <srinivas@lanciussolutions.com>
2017-08-16 12:57:46 +0530

    cart changes

--- cart.php
+++ cart.php
@@ -1,5 +1,8 @@
 <?php include_once 'main_header.php'; ?>
-<?php $session_mobile = $_SESSION['session_mobile']; ?>
+<?php $session_mobile = $_SESSION['session_mobile']; 
+      $sql1="SELECT id,product_id,product_price,product_quantity,product_total_price,user_mobile from cart WHERE user_mobile='$session_mobile'";
+      $res=$conn->query($sql1);
+?>
                 
         <!-- SUB-HEADER area -->
         <div class="pm-sub-header-container pm-parallax-panel" data-stellar-background-ratio="0.5" data-stellar-vertical-offset="0">
@@ -19,50 +22,54 @@
             <div class="row">
             
                 <div class="col-lg-12">
-                    
+                    <?php $sub_total=0; if ($res->num_rows > 0) { ?>
                     <div class="pm-cart-info-container">
+
+                        <div class="col-lg-12">
+                            <div class="pm-cart-info-title">
+                              <h3>You have <?php echo $res->num_rows;  ?> item in your cart</h3>
+                            </div>
+                        </div>
                         
-                        <div class="row">
-                        
-                            <div class="col-lg-12">
-                                <div class="pm-cart-info-title">
-                                  <h3>You have 1 item in your cart</h3>
+                        <?php while($getProductsData = $res->fetch_assoc()) { 
+                              $getProData =  getIndividualDetails($getProductsData['product_id'],'products','id');
+                              $sub_total +=$getProData['product_price'];
+                         ?>
+                            <div class="row">
+                            
+                                <div class="col-lg-2 col-md-2 col-sm-2 pm-cart-info-column">
+                                    <img src="<?php echo $base_url . 'uploads/product_images/'.$getProData['product_image'] ?>" height="50" width="50" /> 
                                 </div>
-                            </div>
-                        
-                            <div class="col-lg-2 col-md-2 col-sm-2 pm-cart-info-column">
-                                <img src="img/store/item.jpg" width="50" height="50"> 
-                            </div>
+                                
+                                <div class="col-lg-2 col-md-2 col-sm-2 pm-cart-info-column text">
+                                    <a href="#"><?php echo $getProData['product_name']?></a>
+                                </div>
+                                
+                                <div class="col-lg-2 col-md-2 col-sm-2 pm-cart-info-column text">
+                                    <p>Price: &#2352; <?php echo $getProductsData['product_price']; ?></p>
+                                </div>
+                                
+                                <div class="col-lg-2 col-md-2 col-sm-2 pm-cart-info-column quantity">
+                                    <div class="quantity buttons_added pm-checkout-quantity">
+                                        <input type="number" size="4" class="input-text qty cart text" title="Qty" value="1" name="quantity" min="1" step="1" >                                                                                                
+                                    </div><!-- quantity buttons end -->
+                                </div>
+                                
+                                <div class="col-lg-2 col-md-2 col-sm-2 pm-cart-info-column text">
+                                    <p>Sub-Total: &#2352; <?php echo $getProductsData['product_price']; ?></p>
+                                </div>
+                                
+                                <div class="col-lg-2 col-md-2 col-sm-2 pm-cart-info-column">
+                                    <a href="cart_delete.php?did=<?php echo $getProductsData['id']?>" class="pm-rounded-btn pm-primary pm-cart-remove">Remove</a>
+                                </div>
                             
-                            <div class="col-lg-2 col-md-2 col-sm-2 pm-cart-info-column text">
-                                <a href="#">WOO SINGLE #1</a>
-                            </div>
-                            
-                            <div class="col-lg-2 col-md-2 col-sm-2 pm-cart-info-column text">
-                                <p>Price: $9</p>
-                            </div>
-                            
-                            <div class="col-lg-2 col-md-2 col-sm-2 pm-cart-info-column quantity">
-                                <div class="quantity buttons_added pm-checkout-quantity">
-                                    <input type="number" size="4" class="input-text qty cart text" title="Qty" value="1" name="quantity" min="1" step="1" >
-                                    <div class="pm-quantity-btns-container">
-                                        <input type="button" class="minus" value="-">
-                                        <input type="button" class="plus" value="+">
-                                    </div>                                                            
-                                </div><!-- quantity buttons end -->
-                            </div>
-                            
-                            <div class="col-lg-2 col-md-2 col-sm-2 pm-cart-info-column text">
-                                <p>Sub-Total: $9</p>
-                            </div>
-                            
-                            <div class="col-lg-2 col-md-2 col-sm-2 pm-cart-info-column">
-                                <a href="#" class="pm-rounded-btn pm-primary pm-cart-remove">Remove</a>
-                            </div>
-                        
-                        </div><!-- /.row -->
+                            </div><!-- /.row -->
+                        <?php } ?>
                         
                     </div><!-- /.pm-cart-info-container -->
+                    <?php  } else { ?>
+                        No Items Found
+                    <?php } ?>
                     
                 </div><!-- /.col-lg-12 -->
             
@@ -91,7 +98,7 @@
                             
                             <div class="col-lg-8 col-md-6 col-sm-6">
                                 <div class="pm-cart-totals-column">
-                                    <p>$9</p>
+                                    <p>&#2352; <?php echo $sub_total; ?></p>
                                 </div>
                             </div>
                         
@@ -107,7 +114,7 @@
                             
                             <div class="col-lg-8 col-md-6 col-sm-6">
                                 <div class="pm-cart-totals-column">
-                                    <p>$7.99</p>
+                                    <p>&#2352; <?php echo $shipping=5; ?></p>
                                 </div>
                             </div>
                         
@@ -123,7 +130,7 @@
                             
                             <div class="col-lg-8 col-md-6 col-sm-6">
                                 <div class="pm-cart-totals-column">
-                                    <p>$16.99</p>
+                                    <p>&#2352; <?php echo $sub_total +$shipping; ?></p>
                                 </div>
                             </div>
                         
