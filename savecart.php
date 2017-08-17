dd1b69e8f71cc134ce79e6d3f1a0ca883406ea40
Srinivaslancius <srinivas@lanciussolutions.com>
2017-08-17 13:16:22 +0530

    checkout page chnaghes

--- savecart.php
+++ savecart.php
@@ -11,8 +11,8 @@ if(!empty($_POST['product_id']) && !empty($_POST['product_price']))  {
 	$sql1= "SELECT * from cart where product_id='$product_id' AND user_mobile='$user_mobile'";
 	$res = $conn->query($sql1);
 	if ($res->num_rows > 0) {
-		echo "<script>alert('Item already added in your cart');window.location.href='index.php';</script>";
-	}else {
+		echo "<script>alert('Item already added in your cart');</script>";
+	} else {
 		$sql = "INSERT INTO cart (`product_id`, `product_price`,  `product_quantity`,  `product_total_price`, `user_mobile`, `created_at`) VALUES ('$product_id', '$product_price', '1', '$product_price', '$user_mobile',  '$date')";
 		if($conn->query($sql) === TRUE){
 			$succ = "1";
