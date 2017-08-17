53166e8cdfe5698fdf8b26b255a3ef8adbd861f8
Srinivaslancius <srinivas@lanciussolutions.com>
2017-08-17 19:25:23 +0530

    orders backend

--- admin/orders.php
+++ admin/orders.php
@@ -40,9 +40,10 @@
                                             <div class="modal-dialog" Style="margin-top:10%;">
                                                 <div class="modal-content">
                                                     <div class="modal-header">
-                                                    <h3 class="modal-title"><b>User Information</b></h3>
+                                                    <h3 class="modal-title"><b>Order Information</b></h3>
                                                     </div>
                                                     <div class="modal-body" >
+                                                    
                                                         <h5 class="modal-title-set"><b>Name:</b><?php echo $res1['first_name'];?></h5>
                                                         <h5 class="modal-title-set"><b>Order Id :</b><?php echo $res1['order_id']."<br>"; ?></h5>
                                                         <h5 class="modal-title-set"><b>Mobile :</b><?php echo $res1['mobile']."<br>"; ?></h5>
@@ -51,6 +52,14 @@
                                                         <h5 class="modal-title-set"><b>Address :</b><?php echo $res1['address1']."<br>"; ?></h5>
                                                         <h5 class="modal-title-set"><b>Pin Code :</b><?php echo $res1['pin_code']."<br>"; ?></h5>
                                                         <h5 class="modal-title-set"><b>Status :</b><?php if ($res1['order_status']==1) { echo "Pending" ;} elseif($res1['order_status']==2) { echo "Completed" ;} else{ echo "Cancelled";}?></h5>
+                                                    <p style="text-align:center; font-weight:bold;">Product Information</p>
+                                                    <?php $proInfo = getAllDataWhere('orders','order_id',$res1['order_id']); ?>
+                                                    <?php while($getAllProInfo = $proInfo->fetch_assoc()) { ?>
+                                                    <p> <?php echo $getAllProInfo['product_name']; ?> - <?php echo $getAllProInfo['product_quantity']; ?> - <?php echo $getAllProInfo['product_price']; ?>- <?php echo $getAllProInfo['product_total_price']; ?></p>
+                                                    <?php } ?>
+                                                    <p>Sub Total : <?php echo $res1['cart_sub_total']."<br>"; ?></p>
+                                                    <p>Shipping and Handling : <?php echo $getSiteSettingsData['delivery_charges']."<br>"; ?></p>
+                                                    <p>Order Total : <?php echo $res1['order_total']."<br>"; ?></p>
                                                     </div>
                                                     <div class="modal-footer" >
                                                           <button type="button" class="btn" data-dismiss="modal" style="background-color:#f00; color:#fff">Close</button>
