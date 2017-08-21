00a180a704656bb18913d2859cf53d4bd13e2235
Srinivaslancius <srinivas@lanciussolutions.com>
2017-08-21 12:36:55 +0530

    bug sheet issues

--- book-an-event.php
+++ book-an-event.php
@@ -22,6 +22,7 @@ if (!isset($_POST['submit']))  {
     }
 }
 ?>            
+<?php $getAboutData = getIndividualDetails('1',"content_pages","id");  ?>   
         <!-- SUB-HEADER area -->
         <div class="pm-sub-header-container pm-parallax-panel" data-stellar-background-ratio="0.5" data-stellar-vertical-offset="0">
         	
@@ -45,9 +46,7 @@ if (!isset($_POST['submit']))  {
                     
                     <p>Sampradaya Ruchulu can accomodate up to 50 people in our private dining area. </p>
                     
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus sit amet purus consectetur, rhoncus urna eget, semper sapien. Integer vehicula, dolor gravida lobortis consectetur, lorem elit mollis magna, vitae rutrum velit nulla id dui. </p>
-                    
-                    <p>Morbi eu mattis justo, vitae porta orci. Sed et est a elit ultrices posuere. Fusce nibh enim, sollicitudin sed orci vitae, mollis elementum risus. Aliquam hendrerit, dui quis tincidunt ultrices, purus nisl aliquet enim, at eleifend nisi ante et justo.</p>
+                    <?php echo $getAboutData['description']; ?>
                     
                     <a href="#catering-form" class="pm-rounded-btn pm-primary pm-page-scroll">Book your event now &nbsp;<i class="fa fa-angle-down"></i></a>
                     
